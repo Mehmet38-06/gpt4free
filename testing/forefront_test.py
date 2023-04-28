a5da482a79635601ed43beaffcc17bf8e0f71f47
Mehmet38-06 <111025630+Mehmet38-06@users.noreply.github.com>
2023-04-28 22:48:32 +0300

    Update forefront_test.py

--- testing/forefront_test.py
+++ testing/forefront_test.py
@@ -7,4 +7,4 @@ print(token)
 # get a response
 for response in forefront.StreamingCompletion.create(token=token,
                                                      prompt='hello world', model='gpt-4'):
-    print(response.completion.choices[0].text, end='')
+    print(response.completion.choices[0].text, end="")
